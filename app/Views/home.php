243271c39c590c1a6f37de5771c9518aba29d42d
Piki <alexandradesplan@gmail.com>
2023-11-16 12:35:37 +0100

    changement du style au niveau de la page d'accueil

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,21 +2,23 @@
     <h1 class="display-4 m-5">
         Bienvenue sur mon CV!
     </h1>
-    <div class="row m-5">
-        <div class="col m-5">
+    <div class="col">
+        <!--image au centre-->
+        <div class="row justify-content-center">
             <img 
                 src="<?php echo base_url('assets/images/profil.jpg')?>" 
                 alt="Alexandra Desplan" 
-                class="img-fluid rounded-circle"
+                class="img-fluid rounded-circle w-25"
             >
         </div>
 
-        <div class="col m-5 align-self-center">
+        <div class="row justify-content-center mt-5">
             <h2 class="display-6">
                 Profil
             </h2>
 
-            <p class="text-justify">
+                <!--texte justifié-->
+            <p class="text-justify mt-3">
                 Je m'appelle Alexandra Desplan, j'ai 20 ans et je suis actuellement en 3ème année de BUT Informatique au Havre. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptatem tempore voluptatum saepe maiores tempora, similique reiciendis mollitia quia ratione minus, blanditiis nulla unde dolorem aut hic possimus voluptas quam.
             </p>
         </div>
