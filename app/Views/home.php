2c56cb7e6f8abf9700b08b8e33e2987d574d413a
Piki <alexandradesplan@gmail.com>
2023-12-03 17:50:43 +0100

    finalisation page d'accueil

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,26 +1,32 @@
-<div class="container text-center">
+<div class="container text-center mb-5 pb-5 pt-5">
     <h1 class="display-4 m-5">
         Bienvenue sur mon CV!
     </h1>
-    <div class="col">
-        <!--image au centre-->
-        <div class="row justify-content-center">
-            <img 
-                src="<?php echo base_url('assets/images/profil.jpg')?>" 
-                alt="Alexandra Desplan" 
-                class="img-fluid rounded-circle w-25"
-            >
-        </div>
+    <div class="col mb-5">
+
+        
+        <h2 class="display-6 justify-content-center">
+            Profil
+        </h2>
 
         <div class="row justify-content-center mt-5">
-            <h2 class="display-6">
-                Profil
-            </h2>
+            <div class="col-4">
+                <img class="img-fluid rounded-circle w-50" 
+                src="assets/images/me.jpg" alt="Photo de profil">
+            </div>
+            
+            <div class="col-6">
+                <p class="lead">
+                    Je m'appelle Alexandra Desplan, j'ai 20 ans et je suis actuellement en 3ème année de BUT Informatique au Havre. 
+                    Je souhaite développer mes compétences au sein de votre entreprise dans le secteur informatique par le biais d'un
+                    stage de 16 semaines qui débuterait en février 2024. A travers ce site web, vous pourrez découvrir mon parcours, mes compétences,
+                    mes expériences professionnelles et mes projets. Vous pouvez également télécharger mon CV en cliquant sur le bouton ci-dessous.
+                </p>
 
-                <!--texte justifié-->
-            <p class="text-justify mt-3">
-                Je m'appelle Alexandra Desplan, j'ai 20 ans et je suis actuellement en 3ème année de BUT Informatique au Havre. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptatem tempore voluptatum saepe maiores tempora, similique reiciendis mollitia quia ratione minus, blanditiis nulla unde dolorem aut hic possimus voluptas quam.
-            </p>
+                <div class="col-12 mt-5 mb-5">
+                <a href="assets/images/CV.pdf" class="btn btn-secondary btn-sm" role="button" aria-pressed="true">Télécharger mon CV</a>
+                </div>
+            </div>
         </div>
 
     </div>
