4e38144ff6ca2210f0e6215e8ff42499df0d0543
Piki <alexandradesplan@gmail.com>
2023-12-03 17:49:42 +0100

    finalisation page compétences

--- app/Views/competences.php
+++ app/Views/competences.php
@@ -1,1 +1,111 @@
-<p> Ceci est un test pour la page des compétences</p>
+<div class="container-fluid">
+    <h2 class="text-center mb-4 text-primary">Compétences</h2>
+    <div class=" row shadow-sm p-3 mb-5 bg-white rounded col-12 col-md-10 offset-md-1">
+        <h4 class="title mb-4 mt-5 text-secondary">Hard-Skills</h4>
+
+        <div class="col-12 text-center mb-4">
+            <h5 class="text-center" style="color: #6c757d;">Langages</h5>
+        </div>
+
+        <div class="row d-flex justify-content-center mb-5">
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/java-coffee-cup-logo.png" class="img-fluid" alt="Java"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">Java</h6>
+            </div>
+
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/html-5.png" class="img-fluid" alt="HTML5"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">HTML5</h6>
+            </div>
+
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/css3.png" class="img-fluid" alt="CSS3"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">CSS3</h6>
+            </div>
+
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/javascript.png" class="img-fluid" alt="JavaScript"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">JavaScript</h6>
+
+            </div>
+
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/php.png" class="img-fluid" alt="PHP"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">PHP</h6>
+
+            </div>
+
+            <div class="col-12 mb-4 mt-5">
+                <h5 class="text-center" style="color: #6c757d;">Frameworks</h5>
+            </div>
+
+            <div class="col-12 d-flex justify-content-center mb-5">
+                <div class="col-6 col-md-3 text-center mb-4">
+                    <img src="https://img.icons8.com/color/96/000000/bootstrap.png" class="img-fluid" alt="Bootstrap"
+                        style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                    <h6 style="margin-top: 10px; font-size: 1rem;">Bootstrap</h6>
+                </div>
+
+                <div class="col-6 col-md-3 text-center mb-4">
+                    <img src="https://img.icons8.com/color/96/000000/react-native.png" class="img-fluid" alt="React"
+                        style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                    <h6 style="margin-top: 10px; font-size: 1rem;">React</h6>
+                </div>
+
+            </div>
+
+            <div class="col-12 text-center mb-4 mt-5">
+                <h5 style="color: #6c757d;">Base de données</h5>
+            </div>
+
+            <div class="col-12 d-flex justify-content-center mb-5">
+                <div class="col-6 col-md-3 text-center mb-4">
+                    <img src="https://img.icons8.com/color/96/000000/postgreesql.png" class="img-fluid" alt="PostgreSQL"
+                        style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                    <h6 style="margin-top: 10px; font-size: 1rem;">PostgreSQL</h6>
+                </div>
+
+                <div class="col-6 col-md-3 text-center mb-4">
+                    <img src="https://img.icons8.com/color/96/000000/mongodb.png" class="img-fluid" alt="MongoDB"
+                        style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                    <h6 style="margin-top: 10px; font-size: 1rem;">MongoDB</h6>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class=" row shadow-sm p-3 mb-7 bg-white rounded col-12 col-md-10 offset-md-1">
+        <h4 class="title mb-4 mt-5 text-secondary">Soft-Skills</h4>
+
+
+        <div class="row d-flex justify-content-center mb-5">
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/communication.png" class="img-fluid"
+                    alt="Communication"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">Communication</h6>
+            </div>
+
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/teamwork.png" class="img-fluid" alt="Travail d'équipe"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">Travail d'équipe</h6>
+            </div>
+
+            <div class="col-6 col-md-3 text-center mb-4">
+                <img src="https://img.icons8.com/color/96/000000/drawing.png" class="img-fluid" alt="Créativité"
+                    style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
+                <h6 style="margin-top: 10px; font-size: 1rem;">Créativité</h6>
+            </div>
+        </div>
+    </div>
+</div>
