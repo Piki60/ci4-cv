710edd394948a7869bededbb318c9709bad05428
Piki <alexandradesplan@gmail.com>
2023-12-03 17:49:28 +0100

    finalisation page experience (logo + contenu +style)

--- app/Views/experiences.php
+++ app/Views/experiences.php
@@ -1,1 +1,52 @@
-<p> Ceci est un test pour la page d'expériences</p>
+<div class="container">
+    <h2 class="text-center mb-4 text-primary">
+        Expériences professionnelles
+    </h2>
+
+    <div>
+        <div class="row d-flex justify-content-center mb-5">
+
+            <h4 class="title mb-5 text-secondary">
+                Expériences en informatique
+            </h4>
+
+            <div class="card mb-5" style="width: 30rem;">
+
+                <div class="bg-secondary">
+
+                    <img src="<?= base_url('/assets/images/logo-attisport.png') ?>"
+                        class="card-img-top w-50 mx-auto d-block" alt="attisport logo">
+
+                </div>
+
+                <div class="card-body">
+                    <h5 class="card-title">Développeur application web chez Attineos</h5>
+                    <p class="card-text">Stage de 8 semaines, projet Interne : site web en ReactJS/Laravel permettant de
+                        parier sur des matchs</p>
+                    <hr>
+                    <p class="card-text">Conception, Implémentation base de données, Implémentation API, Implémentation
+                        Front-End</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row d-flex justify-content-center">
+
+            <h4 class="title mb-5 text-secondary">
+                Autres expériences
+            </h4>
+
+            <div class="card mb-5" style="width: 30rem;">
+                <img src="<?= base_url('/assets/images/logoTotalEnergies.png') ?>" class="card-img-top w-50 mx-auto"
+                    alt="Logo TotalEnergies">
+                <div class="card-body">
+                    <h5 class="card-title">Employée polyvalente dans une station service TotalEnergies</h5>
+                    <p class="card-text">Emploi saisonnier de 2 mois</p>
+                    <p class="card-text">Accueil client, Encaissement, Gestion des stocks, Nettoyage des locaux</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
